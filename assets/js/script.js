902ee3d5b80d8602473a16c847bcc702d96c879d
inayatullahaaxioned <inayatullaha@axioned.com>
2022-11-13 20:30:16 +0530

    js logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 /* Author: 
-
+Inayatullah
 */
-
-
+// accordian js start
+let list = document.querySelectorAll('.data-item');
+list.forEach(function (tab){
+    tab.addEventListener('click', function (){
+        let prevOpen = document.querySelector('.data-item.active');
+        if(this.classList.contains('active')){
+            this.classList.remove('active');
+        }else{
+             if(prevOpen){
+            prevOpen.classList.remove('active');
+        }
+        tab.classList.add('active');
+    }
+    });
+});
+// accordian js end
 
 
 
